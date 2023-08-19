77925016e7bca8598623add80bbf5c1f1da39e4b
vanshika308 <vanshikatiwari0308@gmail.com>
2023-08-19 22:27:32 +0530

    added a cancel button

--- src/App.js
+++ src/App.js
@@ -1,48 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
-async function fetchMoviesHandler() {
-  try {
-    const response = await fetch('https://swapi.dev/api/films/');
-
-
-    const data = await response.json();
-
-    const transformedMovies = data.results.map((movieData) => ({
-      id: movieData.episode_id,
-      title: movieData.title,
-      openingText: movieData.opening_crawl,
-      releasedate: movieData.release_date,
-    }));
-
-    return transformedMovies;
-  } catch (error) {
-    console.error('Error fetching movies:', error);
-    return [];
-  }
-}
-
 function App() {
   const [movies, setMovies] = useState([]);
-  const[isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [canceled, setCanceled] = useState(false); // New state for canceling fetch
+  const retryInterval = 5000;
+  let retryTimer = null;
 
-  const fetchMovies = async () => {
+  async function fetchMoviesHandler() {
     setIsLoading(true);
-    const moviesData = await fetchMoviesHandler();
-    setMovies(moviesData);
+    setError(null);
+
+    try {
+      const response = await fetch('https://swapi.dev/api/film');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong......RETRYING');
+      }
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }));
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      if (!canceled) {
+        setError(error.message);
+        retryTimer = setTimeout(fetchMoviesHandler, retryInterval);
+      }
+    }
+
     setIsLoading(false);
+  }
+
+  const handleCancelClick = () => {
+    clearTimeout(retryTimer);
+    setCanceled(true); 
   };
+
+  useEffect(() => {
+    fetchMoviesHandler();
+    return () => {
+      clearTimeout(retryTimer);
+    };
+  }, [canceled]);
+
+  let content = <p>Found no movies!</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading....</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMovies}>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={handleCancelClick}>Cancel Retry</button>
       </section>
-      <section>
-        {!isLoading  && <MoviesList movies={movies} />}
-        {isLoading  && <p>Loading.......</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
